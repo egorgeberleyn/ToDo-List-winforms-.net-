159e45d8c30337e4cdcda4dd633387de066a5211
Egor Geberleyn <egeberleyn@gmail.com>
2022-07-08 23:22:28 +0300

    Добавьте файлы проекта.

---
+++ LimitedSizeStack.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TodoApplication
+{
+    public class LimitedSizeStack<T>
+    {
+        LinkedList<T> list = new LinkedList<T>();
+        public int Limit;
+        public LimitedSizeStack(int limit)
+        {
+            Limit = limit;
+        }
+
+        public void Push(T item)
+        {
+            if (Limit == 0)
+                return;
+            if(list.Count >= Limit)
+            {
+                list.Remove(list.First.Value);
+            }
+            list.AddLast(item);
+            
+        }
+
+        public T Pop()
+        {
+            if (list.Count == 0) throw new InvalidOperationException();
+            var result = list.Last.Value;
+            list.Remove(list.Last.Value);
+            return result;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return list.Count; 
+            }
+        }
+    }
+}
---
+++ ListModel.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+namespace TodoApplication
+{    
+    public class ListModel<TItem>
+    {
+        private struct HistoryItem
+        {
+            public string Action;
+            public TItem Item;
+            public int Index;
+        }
+
+        public List<TItem> Items { get; }
+        public int Limit;
+        private LimitedSizeStack<HistoryItem> history;
+
+        public ListModel(int limit)
+        {
+            Items = new List<TItem>();
+            Limit = limit;
+            history = new LimitedSizeStack<HistoryItem>(limit);
+        }
+
+        public void AddItem(TItem item)
+        {            
+            history.Push(new HistoryItem { Action = "push", Index = Items.Count });
+            Items.Add(item);
+        }
+
+        public void RemoveItem(int index)
+        {
+            history.Push(new HistoryItem { Action = "pop", Item = Items[index], Index = index});
+            Items.RemoveAt(index);
+        }
+
+        public bool CanUndo()
+        {
+            return history.Count != 0;               
+        }
+
+        public void Undo()
+        {           
+            if (CanUndo())
+            {
+                var action = history.Pop();
+                if (action.Action == "pop")
+                    Items.Insert(action.Index, action.Item);
+                else if(action.Action == "push")
+                    Items.RemoveAt(action.Index);
+            }
+        }
+    }
+}
